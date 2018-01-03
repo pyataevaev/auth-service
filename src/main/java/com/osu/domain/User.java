6fae951511e741576b0fd8f72eea673ae4fba4b0
Ekaterina Pyataeva <ekaterina.pyataeva@waveaccess.ru>
2018-01-03 12:52:41 +0500

    Fixed errors during start app with db

--- src/main/java/com/osu/domain/User.java
+++ src/main/java/com/osu/domain/User.java
@@ -1,15 +1,13 @@
 package com.osu.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.hibernate.annotations.*;
-import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.BatchSize;
 import org.hibernate.validator.constraints.Email;
 
 import javax.persistence.*;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.HashSet;
